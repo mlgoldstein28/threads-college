ba5e5d70e438b0af98e7db46af52ce7fef2d87ff
Michael Goldstein <mlgoldstein28@gmail.com>
2023-06-22 11:35:21 -0400

    Threads DB Initialize

---
+++ server/server.mjs
@@ -0,0 +1,16 @@
+import express from 'express';
+import cors from 'cors';
+import './loadEnvironment.mjs';
+import records from './routes/record.mjs';
+
+const PORT = process.env.port || 5060;
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use('/record', records)
+
+app.listen(PORT, () => {
+    console.log(`server is running on port: ${PORT}`)
+})
